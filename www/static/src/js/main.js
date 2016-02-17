814e30daf80078a9f69d8f7b3cb9f14a40c30c01
hisland <hisland@qq.com>
2016-02-18 00:07:12 +0800

    02/18/16 00:07:12 normal commit

--- www/static/src/js/main.js
+++ www/static/src/js/main.js
@@ -8,6 +8,16 @@ var app = angular.module('app', [
 app.controller('show', function($scope) {
   $scope.path = path;
   $scope.list = list;
+
+  $scope.change = function(){
+    
+  }
+  $scope.sort = function(){
+    
+  }
+  $scope.save = function(){
+    
+  }
 });
 
 // init angular
